335d837c244a9b11eb2f93bbe29d910a51e3a9d7
Srini <s@srin.me>
2019-11-09 17:51:56 -0500

    added the version command

---
+++ disign/cmd/cmd.go
@@ -0,0 +1,11 @@
+package cmd
+// C header generator
+// File: cmd.h
+const BUILD_TIME = "Sat Nov 9 2019 17:50:49"
+const VERSION_MAJOR = 0
+const VERSION_MINOR = 0
+const VERSION_BUILD = 999
+const REPO_URL = "git@gitlab.com:privatetutor/projectlets/go.git"
+const BRANCH_NAME = "master"
+const SHORT_COMMIT_ID = "9779773"
+const LONG_COMMIT_ID = "97797734d928f526e3bac04ac6cbb4964d26ea92"
