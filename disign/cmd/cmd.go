0400b74a0750c66dcfaba839a7f4bf70ec6e6006
Srini <s@srin.me>
2020-01-14 11:54:00 -0500

    removed unnecessary license statements.

--- disign/cmd/cmd.go
+++ disign/cmd/cmd.go
@@ -1,11 +1,11 @@
 package cmd
 // Go package generator
 // File: cmd.h
-const buildTime = "Tue Jan 14 2020 11:36:04"
+const buildTime = "Tue Jan 14 2020 11:52:46"
 const versionMajor = 0
-const versionMinor = 0
-const versionBuild = 999
+const versionMinor = 1
+const versionBuild = 1
 const repoURL = "git@gitlab.com:privatetutor/projectlets/go.git"
 const branchName = "master"
-const shortCommitId = "c37e1d4"
-const longCommitId = "c37e1d41d829bf8e58c009180ba5baf0477c5c04"
+const shortCommitId = "d4a3b12"
+const longCommitId = "d4a3b1238517f0667ad93242f5bb51592b5d007a"
