0491f919149cdfbc57e7b79d11fbd5bb041685c9
vagrant <vagrant@ubuntu1904.localdomain>
2020-01-14 21:05:37 +0000

    update version spec

--- disign/cmd/cmd.go
+++ disign/cmd/cmd.go
@@ -1,11 +1,11 @@
 package cmd
 // Go package generator
 // File: cmd.h
-const buildTime = "Tue Jan 14 2020 11:52:46"
+const buildTime = "Tue Jan 14 2020 21:05:02"
 const versionMajor = 0
 const versionMinor = 1
 const versionBuild = 1
-const repoURL = "git@gitlab.com:privatetutor/projectlets/go.git"
+const repoURL = "https://github.com/RajaSrinivasan/disign.git"
 const branchName = "master"
-const shortCommitId = "d4a3b12"
-const longCommitId = "d4a3b1238517f0667ad93242f5bb51592b5d007a"
+const shortCommitId = "0c2bf7f"
+const longCommitId = "0c2bf7f5ad9c844baadb54f1c6f272e821054b7f"
